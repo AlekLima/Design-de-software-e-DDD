7b7d7a32981f91ecd7c1ab6361ae0602b9bf88c6
AlekLima <167828616+AlekLima@users.noreply.github.com>
2024-07-19 17:38:56 -0300

    Commit: Fórum com DDD & Clean Architecture - Configuração do ESLint
    Command Line:
     "npm i eslint @rocketseat/eslint-config -D",
    "npm i eslint-plugin-vitest-globals -D"

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,29 +1,29 @@
-import { UniqueEntityID } from "../../core/entities/unique-entity-id"
-import { Answer } from "../entities/answer"
-import { AnswersRepository } from "../repositories/answers-repository"
+import { UniqueEntityID } from '../../core/entities/unique-entity-id'
+import { Answer } from '../entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
 
 interface AnswerQuestionUseCaseRequest {
-    InstructorId: string
-    questionId: string
-    content: string
-     
+  InstructorId: string
+  questionId: string
+  content: string
 }
 
 export class AnswerQuestionUseCase {
-    constructor(
-        private answersRepository: AnswersRepository,
-    ) {}
+  constructor(private answersRepository: AnswersRepository) {}
 
-    async execute({ InstructorId, questionId, content }: AnswerQuestionUseCaseRequest) {      
-        const answer = Answer.create({
-            content,
-            authorId: new UniqueEntityID(InstructorId),
-            questionId: new UniqueEntityID(questionId)
-        })
+  async execute({
+    InstructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest) {
+    const answer = Answer.create({
+      content,
+      authorId: new UniqueEntityID(InstructorId),
+      questionId: new UniqueEntityID(questionId),
+    })
 
-        await this.answersRepository.create(answer)
+    await this.answersRepository.create(answer)
 
-        return answer
-    }
+    return answer
+  }
 }
-
